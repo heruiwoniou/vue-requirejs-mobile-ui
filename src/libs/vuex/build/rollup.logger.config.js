9c7ce4392ccd9dcb9facdc9c8a0931c31b19a522
heruiwoniou <heruiwoniou@qq.com>
2017-01-03 11:18:31 +0800

    '提交了内容'

--- src/libs/vuex/build/rollup.logger.config.js
+++
@@ -1,9 +0,0 @@
-const buble = require('rollup-plugin-buble')
-
-module.exports = {
-  entry: 'src/plugins/logger.js',
-  dest: 'dist/logger.js',
-  format: 'umd',
-  moduleName: 'createVuexLogger',
-  plugins: [buble()]
-}
