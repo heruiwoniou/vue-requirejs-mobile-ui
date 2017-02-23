9cf7f1bc44208f7603a9e9d06c91b4cfc2b0f059
heruiwoniou <heruiwoniou@qq.com>
2017-02-23 11:33:35 +0800

    addsettime

--- controller/api.js
+++ controller/api.js
@@ -1,12 +1,14 @@
+var auth = 'logined'
 module.exports = {
     verify(query) {
         var pass = query.username == 'admin' && query.password == 'ab123';
         return this.render.json({
             state: pass,
-            auth: pass ? 'aa' : ''
+            auth: pass ? auth : ''
         })
     },
     auth(query) {
-        return this.render.json(query.auth == 'ba')
+        console.log(auth);
+        return this.render.json(query.auth == auth)
     }
 }
--- src/store/modules/Login/store.js
+++ src/store/modules/Login/store.js
@@ -22,7 +22,12 @@ define(function() {
         this.actions = {
             verify: function(content, opt) {
                 return new Promise(function(resolve, reject) {
-                    $.get('/api/verify', { username: opt.username, password: opt.password }).then(function(data) {
+                    setTimeout(function() {
+                        var data = {
+                            state: true,
+                            auth: 'aa'
+                        }
+
                         if (data.state) {
                             content.commit('success', data.auth);
                             resolve();
@@ -30,15 +35,27 @@ define(function() {
                             content.commit('fail');
                             reject();
                         }
-                    }, function() {
-                        content.commit('fail');
-                        reject("服务器错误！请联系管理员");
-                    });
+
+                    }, 2000);
+                    // $.get('/api/verify', { username: opt.username, password: opt.password }).then(function(data) {
+                    //     if (data.state) {
+                    //         content.commit('success', data.auth);
+                    //         resolve();
+                    //     } else {
+                    //         content.commit('fail');
+                    //         reject();
+                    //     }
+                    // }, function() {
+                    //     content.commit('fail');
+                    //     reject("服务器错误！请联系管理员");
+                    // });
                 })
             },
             auth: function(content) {
                 return new Promise(function(resolve, reject) {
-                    $.get('/api/auth', { auth: content.state.auth }).then(function(data) {
+                    setTimeout(function() {
+                        var data = "aa";
+
                         if (data) {
                             content.commit('success');
                             resolve(data);
@@ -46,7 +63,16 @@ define(function() {
                             content.commit('fail');
                             reject();
                         }
-                    });
+                    }, 2000);
+                    // $.get('/api/auth', { auth: content.state.auth }).then(function(data) {
+                    //     if (data) {
+                    //         content.commit('success');
+                    //         resolve(data);
+                    //     } else {
+                    //         content.commit('fail');
+                    //         reject();
+                    //     }
+                    // });
                 });
             }
         }
